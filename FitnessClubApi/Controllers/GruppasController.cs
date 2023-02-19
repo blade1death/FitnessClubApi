87cba3c1ae75f36ce154faeaa1342c6109ed5de0
Сергей Разжевалов <dragonking.12235@mail.ru>
2023-02-19 12:06:47 +0300

    Создание оставшихся контроллеров

---
+++ FitnessClubApi/Controllers/GruppasController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FitnessClubApi.Models;
+
+namespace FitnessClubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GruppasController : ControllerBase
+    {
+        private readonly TestContext _context;
+
+        public GruppasController(TestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Gruppas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Gruppa>>> GetGruppas()
+        {
+            return await _context.Gruppas.ToListAsync();
+        }
+
+        // GET: api/Gruppas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Gruppa>> GetGruppa(string id)
+        {
+            var gruppa = await _context.Gruppas.FindAsync(id);
+
+            if (gruppa == null)
+            {
+                return NotFound();
+            }
+
+            return gruppa;
+        }
+
+        // PUT: api/Gruppas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGruppa(string id, Gruppa gruppa)
+        {
+            if (id != gruppa.Названиеgruppi)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gruppa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GruppaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Gruppas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Gruppa>> PostGruppa(Gruppa gruppa)
+        {
+            _context.Gruppas.Add(gruppa);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (GruppaExists(gruppa.Названиеgruppi))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetGruppa", new { id = gruppa.Названиеgruppi }, gruppa);
+        }
+
+        // DELETE: api/Gruppas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGruppa(string id)
+        {
+            var gruppa = await _context.Gruppas.FindAsync(id);
+            if (gruppa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gruppas.Remove(gruppa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GruppaExists(string id)
+        {
+            return _context.Gruppas.Any(e => e.Названиеgruppi == id);
+        }
+    }
+}
---
+++ FitnessClubApi/Controllers/RaspisaniesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FitnessClubApi.Models;
+
+namespace FitnessClubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RaspisaniesController : ControllerBase
+    {
+        private readonly TestContext _context;
+
+        public RaspisaniesController(TestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Raspisanies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Raspisanie>>> GetRaspisanies()
+        {
+            return await _context.Raspisanies.ToListAsync();
+        }
+
+        // GET: api/Raspisanies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Raspisanie>> GetRaspisanie(int id)
+        {
+            var raspisanie = await _context.Raspisanies.FindAsync(id);
+
+            if (raspisanie == null)
+            {
+                return NotFound();
+            }
+
+            return raspisanie;
+        }
+
+        // PUT: api/Raspisanies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRaspisanie(int id, Raspisanie raspisanie)
+        {
+            if (id != raspisanie.Identificatorraspisania)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(raspisanie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RaspisanieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Raspisanies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Raspisanie>> PostRaspisanie(Raspisanie raspisanie)
+        {
+            _context.Raspisanies.Add(raspisanie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRaspisanie", new { id = raspisanie.Identificatorraspisania }, raspisanie);
+        }
+
+        // DELETE: api/Raspisanies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRaspisanie(int id)
+        {
+            var raspisanie = await _context.Raspisanies.FindAsync(id);
+            if (raspisanie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Raspisanies.Remove(raspisanie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RaspisanieExists(int id)
+        {
+            return _context.Raspisanies.Any(e => e.Identificatorraspisania == id);
+        }
+    }
+}
